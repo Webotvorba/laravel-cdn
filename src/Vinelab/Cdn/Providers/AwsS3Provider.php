758fbcb3ff21743e92d87909ad6ac4fa676aac9b
Webotvorba <tichon@webotvorba.sk>
2020-02-14 19:16:06 +0100

    Changed getPaginator to getIterator

--- src/Vinelab/Cdn/Providers/AwsS3Provider.php
+++ src/Vinelab/Cdn/Providers/AwsS3Provider.php
@@ -400,7 +400,7 @@ class AwsS3Provider extends Provider implements ProviderInterface
             $fileOnAWS = $filesOnAWS->get(str_replace('\\', '/', $item->getPathName()));
 
             //select to upload files that are different in size AND last modified time.
-            if(!($item->getMTime() === $fileOnAWS['LastModified']) && !($item->getSize() === $fileOnAWS['Size'])) {
+            if( ! ($item->getSize() == $fileOnAWS['Size']) ) {
                 return $item;
             }
         });
